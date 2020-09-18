599ce17f93a195eb801dc4892556d4366dba20ff
trevorism <trevorism@gmail.com>
2020-09-18 00:24:42 -0400

    Fix issue deserializing response

--- src/main/groovy/com/trevorism/gcloud/webapi/model/User.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/model/User.groovy
@@ -9,8 +9,20 @@ class User {
     String username
     String email
     String image
+
+    String password
+    String salt
+
     boolean admin
+
     boolean active
+    Date dateCreated
+    Date dateExpired
+
+    @Override
+    String getIdentifer() {
+        return username
+    }
 
     static User NULL_USER = new User()
     static boolean isNullUser(User user) {
