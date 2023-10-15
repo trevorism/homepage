1134d7f36b7372826eadcc70355a3ca843c4b2fa
Trevor Brooks <trevorism@gmail.com>
2023-10-15 17:12:13 -0400

    Improve swagger docs

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -8,7 +8,10 @@ import io.swagger.v3.oas.annotations.info.Info
 
 @OpenAPIDefinition(
         info = @Info(
-                contact = @Contact(url = "https://trevorism.com")
+                title = "Trevorism",
+                version = "1.0.0",
+                description = "Homepage for Trevorism",
+                contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
 )
 @CompileStatic
