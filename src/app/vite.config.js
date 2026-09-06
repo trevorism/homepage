54eed6c37e279ff59605dddeb26ded1c90502cdc
Trevor Brooks <trevorism@gmail.com>
2026-09-06 09:21:55 -0400

    Also fix local issue with ui migration

--- src/app/vite.config.js
+++ src/app/vite.config.js
@@ -1,25 +1,6 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite";
-import PropertiesReader from 'properties-reader';
-import axios from 'axios';
-
-let token = '';
-try {
-    const properties = PropertiesReader('../main/resources/secrets.properties');
-    const clientId = properties.get('localUser');
-    const clientSecret = properties.get('localPassword');
-    if(clientId && clientSecret) {
-        axios.post('https://auth.trevorism.com/token', {
-            id: clientId,
-            password: clientSecret,
-            type: "user"
-        }).then((response) => {
-            token = response.data;
-        });
-    }
-} catch (e) {
-}
 
 export default defineConfig({
     plugins: [tailwindcss(), vue()],
@@ -28,7 +9,8 @@ export default defineConfig({
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:8080/',
-                changeOrigin: true,
+                changeOrigin: false,
+                xfwd: true,
                 secure: false,
                 configure: (proxy, _options) => {
                     proxy.on('error', (err, _req, _res) => {
@@ -36,12 +18,9 @@ export default defineConfig({
                     });
                     proxy.on('proxyReq', (proxyReq, req, _res) => {
                         console.log('Sending Request to the Target:', req.method, req.url);
-                        proxyReq.setHeader('Cookie', `session=${token}`);
                     });
                     proxy.on('proxyRes', (proxyRes, req, _res) => {
                         console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-                        const expires = new Date(new Date().getTime() + 60 * 15 * 1000).toUTCString();
-                        _res.setHeader('Set-Cookie', `user_name=test; Path=/; Expires=${expires}`);
                     });
                 }
             },
